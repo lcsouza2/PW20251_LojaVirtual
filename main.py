2144331cbad7a4ad954201138ea08ebcf0b2369a
Ricardo Maroquio <maroquio@gmail.com>
2025-05-05 21:54:29 -0300

    feat: add product detail page and update main page to display product details

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from babel.numbers import format_currency
 
-from produto_repo import criar_tabela, obter_produtos_por_pagina
+from produto_repo import criar_tabela, obter_produto_por_id, obter_produtos_por_pagina
 
 criar_tabela()
 
@@ -19,6 +19,12 @@ templates.env.filters['format_currency_br'] = format_currency_br
 def read_root(request: Request):
     produtos = obter_produtos_por_pagina(12, 0)
     response = templates.TemplateResponse("index.html", {"request": request, "produtos": produtos})
+    return response
+
+@app.get("/produtos/{id}")
+def read_produto(request: Request, id: int):
+    produto = obter_produto_por_id(id)
+    response = templates.TemplateResponse("produto.html", {"request": request, "produto": produto})
     return response
 
 
